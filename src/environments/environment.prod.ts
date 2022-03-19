6a275f5d90484c9c10a65d03dc9d4e89ab7646f4
Tiemoko Sogodogo <tiemoko.sogodogo@odc.local>
2022-03-19 11:05:03 +0000

    Deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:8080/api'
+  // url: 'http://localhost:8080/api',
+  url: 'https://api-odk-pointage.herokuapp.com/api'
 };
